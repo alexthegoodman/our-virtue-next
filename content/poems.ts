9bae3ccee346b6a043d946fffe51dac0e67b6268
Alex Goodman <alexthegoodman@gmail.com>
2024-07-19 17:50:29 -0400

    init scaffolding + mdx

---
+++ content/poems.ts
@@ -0,0 +1,153 @@
+export const poemList = [
+  {
+    title: "Salvation",
+    items: [
+      { title: "Believe in God", path: "/salvation/believe-in-god" },
+      { title: "Follow Jesus", path: "/salvation/follow-jesus" },
+      { title: "Have Forgiveness", path: "/salvation/have-forgiveness" },
+      { title: "Love One Another", path: "/salvation/love-one-another" },
+      { title: "Be Generous", path: "/salvation/be-generous" },
+      { title: "Have Faith", path: "/salvation/have-faith" },
+      { title: "Call to Him", path: "/salvation/call-to-him" },
+      { title: "Our Mission", path: "/salvation/our-mission" },
+    ],
+  },
+  {
+    title: "Being Together",
+    items: [
+      { title: "Make Peace", path: "/being-together/make-peace" },
+      { title: "Reward Each Other", path: "/being-together/reward-each-other" },
+      { title: "Trust Each Other", path: "/being-together/trust-each-other" },
+      { title: "Be Understanding", path: "/being-together/be-understanding" },
+      { title: "Follow The Doctor", path: "/being-together/follow-the-doctor" },
+      { title: "Blame Yourself", path: "/being-together/blame-yourself" },
+    ],
+  },
+  {
+    title: "The Straight Path",
+    items: [
+      {
+        title: "Practice The Word",
+        path: "/the-straight-path/practice-the-word",
+      },
+      { title: "Be Humble", path: "/the-straight-path/be-humble" },
+      {
+        title: "Respect Your Family",
+        path: "/the-straight-path/respect-your-family",
+      },
+      { title: "Speak Wisely", path: "/the-straight-path/speak-wisely" },
+      {
+        title: "Judge Righteously",
+        path: "/the-straight-path/judge-righteously",
+      },
+      { title: "Walk Straight", path: "/the-straight-path/walk-straight" },
+      {
+        title: "Control Yourself",
+        path: "/the-straight-path/control-yourself",
+      },
+      { title: "Act Honorably", path: "/the-straight-path/act-honorably" },
+      { title: "Use Moderation", path: "/the-straight-path/use-moderation" },
+      { title: "Be Grateful", path: "/the-straight-path/be-grateful" },
+      { title: "Work In Peace", path: "/the-straight-path/work-in-peace" },
+      { title: "Act In Secret", path: "/the-straight-path/act-in-secret" },
+    ],
+  },
+  {
+    title: "The Way",
+    items: [
+      { title: "Blessed Costs", path: "/the-way/blessed-costs" },
+      { title: "Spread The Word", path: "/the-way/spread-the-word" },
+      { title: "Mind The Times", path: "/the-way/mind-the-times" },
+      { title: "Seek Not Earth", path: "/the-way/seek-not-earth" },
+      { title: "Do Good", path: "/the-way/do-good" },
+      { title: "Go His Way", path: "/the-way/go-his-way" },
+      { title: "Hope For A New Life", path: "/the-way/hope-for-a-new-life" },
+      { title: "Fear Him", path: "/the-way/fear-him" },
+    ],
+  },
+  {
+    title: "The People",
+    items: [
+      { title: "His Servants", path: "/the-people/his-servants" },
+      { title: "One Book", path: "/the-people/one-book" },
+      { title: "One Church", path: "/the-people/one-church" },
+    ],
+  },
+  {
+    title: "Authority",
+    items: [
+      { title: "Claim God's House", path: "/authority/claim-gods-house" },
+      { title: "The Law-Bringer", path: "/authority/the-law-bringer" },
+      { title: "Like A King", path: "/authority/like-a-king" },
+      { title: "Penalty And Mercy", path: "/authority/penalty-and-mercy" },
+      {
+        title: "Never Violate Another",
+        path: "/authority/never-violate-another",
+      },
+    ],
+  },
+  {
+    title: "His Design",
+    items: [
+      { title: "Have Courage", path: "/his-design/have-courage" },
+      { title: "Choose Freely", path: "/his-design/choose-freely" },
+      { title: "Value The Weight", path: "/his-design/value-the-weight" },
+    ],
+  },
+  {
+    title: "Substances and Health",
+    items: [
+      { title: "Stay Healthy", path: "/substances-and-health/stay-healthy" },
+      {
+        title: "Don't Love Poison",
+        path: "/substances-and-health/dont-love-poison",
+      },
+      { title: "Use Right", path: "/substances-and-health/use-right" },
+    ],
+  },
+  {
+    title: "Suffering",
+    items: [
+      { title: "Endure Affliction", path: "/suffering/endure-affliction" },
+      { title: "Escape The Fire", path: "/suffering/escape-the-fire" },
+      { title: "Cry No More", path: "/suffering/cry-no-more" },
+      { title: "Be Not Weak", path: "/suffering/be-not-weak" },
+      { title: "Beware Of Carnage", path: "/suffering/beware-of-carnage" },
+    ],
+  },
+  {
+    title: "Worry and Painful Thoughts",
+    items: [
+      {
+        title: "Peaceful Thoughts",
+        path: "/worry-and-painful-thoughts/peaceful-thoughts",
+      },
+      {
+        title: "Peace With Less",
+        path: "/worry-and-painful-thoughts/peace-with-less",
+      },
+      {
+        title: "Dark Feelings",
+        path: "/worry-and-painful-thoughts/dark-feelings",
+      },
+      { title: "Temptations", path: "/worry-and-painful-thoughts/temptations" },
+    ],
+  },
+  {
+    title: "Evildoers and Wicked People",
+    items: [
+      {
+        title: "Being Hated",
+        path: "/evildoers-and-wicked-people/being-hated",
+      },
+      {
+        title: "Evil People",
+        path: "/evildoers-and-wicked-people/evil-people",
+      },
+      {
+        title: "The Slave Owner",
+        path: "/evildoers-and-wicked-people/the-slave-owner",
+      },
+    ],
+  },
+];
