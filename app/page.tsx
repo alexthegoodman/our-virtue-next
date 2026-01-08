4c208d572da57ad5de407658cee7f5146f095572
Alex Woodman <alexthegoodman@gmail.com>
2026-01-07 21:49:36 -0500

    add suspense boundary

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import SwipeInterface from "@/components/SwipeInterface";
 import stanzaData from "../content/stanzas.json";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default async function Home() {
   // const filePath = path.join(process.cwd(), 'content', 'stanzas.json');
@@ -12,7 +13,9 @@ export default async function Home() {
   return (
     <>
       <main style={{ height: '100%', minHeight: "76vh", width: '100%', overflow: 'hidden', position: 'relative' }}>
-        <SwipeInterface data={stanzaData} />
+        <Suspense fallback={<div>Loading poems...</div>}>
+          <SwipeInterface data={stanzaData} />
+        </Suspense>
       </main>
       <div style={{ margin: "20px auto", width: "300px", display: "flex", flexDirection: "column", gap: "12px" }}>
         <Link href="/summary" style={{ color: "#666" }}>Summary and Explore by Category</Link>
