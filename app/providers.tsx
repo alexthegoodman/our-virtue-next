968afb4cc700151532f5f81da2aa77e6ba9e7f59
Alex Woodman <alexthegoodman@gmail.com>
2026-08-23 16:40:47 -0400

    add posthog

--- app/providers.tsx
+++ app/providers.tsx
@@ -17,10 +17,33 @@ export function ClientProviders({ children }: { children: ReactNode }) {
   let router = useRouter();
 
   return (
-    <RouterProvider navigate={router.push}>
-      <AuthProvider>
-        {children}
-      </AuthProvider>
-    </RouterProvider>
+    <PostHogProvider>
+      <RouterProvider navigate={router.push}>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </RouterProvider>
+    </PostHogProvider>
   );
+}
+
+import { usePathname, useSearchParams } from "next/navigation"
+import { useEffect } from "react"
+
+import posthog from 'posthog-js'
+import { PostHogProvider as PHProvider } from '@posthog/react'
+
+function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN as string, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      defaults: '2026-05-30'
+    })
+  }, [])
+
+  return (
+    <PHProvider client={posthog}>
+      {children}
+    </PHProvider>
+  )
 }
